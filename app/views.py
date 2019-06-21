b3a5286e846a81ee711ed93c42d49f161e029561
feven <feven.m.woldu@gmail.com>
2019-06-21 17:10:11 +0300

    added the  index and the news functions, when doing routing

--- app/views.py
+++ app/views.py
@@ -8,4 +8,13 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    message='Home-welcome to the best News Highlights online'
+    return render_template('index.html',message=message)
+
+@app.route('/news/<int:news_id>')
+def news(news_id):
+
+    '''
+    View Newse page function that returns the News details page and its data
+    '''
+    return render_template('news.html',id = news_id)
