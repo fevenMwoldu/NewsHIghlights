d23886084ea238de19a54c9952e5d78e55f9c2ad
feven <feven.m.woldu@gmail.com>
2019-06-23 17:41:42 +0300

    included the ARTICLES_API_URL

---
+++ app/config.py
@@ -0,0 +1,28 @@
+class Config:
+    '''
+    General configuration parent class
+    '''
+    SOURCES_API_URL ='https://newsapi.org/v2/sources?apiKey={}'
+    ARTICLES_API_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
+
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
